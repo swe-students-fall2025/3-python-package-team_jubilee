b16539a1c5a44b8ed9bd3797e8d46c91b8bb99e6
gracehe04 <gh2313@nyu.edu>
2025-11-04 20:06:10 -0500

    github actions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="animalsay",
+    version="0.1.0",
+    packages=find_packages(where="src"),  # <-- look in src/
+    package_dir={"": "src"},               # <-- maps packages from src/
+    install_requires=[
+        "pytest",
+    ],
+)
